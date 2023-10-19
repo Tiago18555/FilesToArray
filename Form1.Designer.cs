79cf229ab156543959a701a691f01461568952ed
Tiago18555 <tcajueiro_1995@hotmail.com>
2023-10-18 21:21:15 -0300

    ver1

---
+++ Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace FilesToArray
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDiag = new System.Windows.Forms.FolderBrowserDialog();
+            this.rtbOutput = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnOpenFile = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // openFileDiag
+            // 
+            this.openFileDiag.HelpRequest += new System.EventHandler(this.openFileDiag_HelpRequest);
+            // 
+            // rtbOutput
+            // 
+            this.rtbOutput.Location = new System.Drawing.Point(12, 70);
+            this.rtbOutput.Name = "rtbOutput";
+            this.rtbOutput.Size = new System.Drawing.Size(776, 319);
+            this.rtbOutput.TabIndex = 0;
+            this.rtbOutput.Text = "";
+            this.rtbOutput.TextChanged += new System.EventHandler(this.rtbOutput_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(737, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "OUTPUT";
+            // 
+            // btnOpenFile
+            // 
+            this.btnOpenFile.Location = new System.Drawing.Point(15, 33);
+            this.btnOpenFile.Name = "btnOpenFile";
+            this.btnOpenFile.Size = new System.Drawing.Size(75, 23);
+            this.btnOpenFile.TabIndex = 2;
+            this.btnOpenFile.Text = "&OPEN FILE";
+            this.btnOpenFile.UseVisualStyleBackColor = true;
+            this.btnOpenFile.Click += new System.EventHandler(this.btnOpenFile_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 401);
+            this.Controls.Add(this.btnOpenFile);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.rtbOutput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private FolderBrowserDialog openFileDiag;
+        private RichTextBox rtbOutput;
+        private Label label1;
+        private Button btnOpenFile;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,59 @@
+using System.Diagnostics;
+using System.Security;
+using System.Windows.Forms;
+
+namespace FilesToArray
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openFileDiag_HelpRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        private void rtbOutput_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnOpenFile_Click(object sender, EventArgs e)
+        {
+            if (openFileDiag.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    string folderPath = openFileDiag.SelectedPath;
+                    string[] files = Directory.GetFiles(folderPath);
+
+                    rtbOutput.Clear();
+                    rtbOutput.Text += "[\n";
+
+                    foreach (string file in files)
+                    {
+                        string fileName = Path.GetFileName(file);
+                        rtbOutput.Text += $"\"./sample_data/{fileName}\",\n";
+                    }
+
+                    rtbOutput.Text = rtbOutput.Text.Remove(rtbOutput.Text.Length - 2);
+                    rtbOutput.Text += "\n]";
+
+                    MessageBox.Show("Texto copiado para área de transferência");
+
+                    rtbOutput.ForeColor = Color.Green;
+
+                    Clipboard.SetText(rtbOutput.Text);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Erro ao acessar a pasta.\n\nMensagem de erro: {ex.Message}\n\n" +
+                                    $"Detalhes:\n\n{ex.StackTrace}");
+                }
+            }
+        }
+    }
+}
